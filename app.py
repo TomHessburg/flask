e7124c3396018b489ac16ac07eb8b0300881e579
Thomas Hessburg <44884308+TomHessburg@users.noreply.github.com>
2019-07-27 18:56:18 -0400

    init

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+
+# init app
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+#database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# initialize db
+db = SQLAlchemy(app)
+# init marchmallow
+ma = Marshmallow(app)
+
+# Product class/model
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_kay=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    qty = db.Column(db.Integer)
+
+    def __init__(self, name, description, price, qty):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.qty = qty
+
+
+# run server
+if __name__ == "__main__":
+    app.run(debug=True)
