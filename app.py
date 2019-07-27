7de1ac9465fe6e888a57e728b7323912ccb0bca0
Thomas Hessburg <44884308+TomHessburg@users.noreply.github.com>
2019-07-27 19:15:54 -0400

    easy enough! haha

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ ma = Marshmallow(app)
 
 # Product class/model
 class Product(db.Model):
-    id = db.Column(db.Integer, primary_kay=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True)
     description = db.Column(db.String(200))
     price = db.Column(db.Float)
@@ -28,6 +28,65 @@ class Product(db.Model):
         self.price = price
         self.qty = qty
 
+class ProductSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'description', 'price', 'qty')
+
+# init schema
+product_schema = ProductSchema(strict=True)
+products_schema = ProductSchema(many=True, strict=True)
+
+# to now set up db, enter 'python' shell and run:
+# >>> from app import db
+# >>> db.create_all()
+
+# routes
+@app.route("/product", methods=['POST'])
+def add_product():
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    qty = request.json['qty']
+    new_product = Product(name, description, price, qty)
+    db.session.add(new_product)
+    db.session.commit()
+    return product_schema.jsonify(new_product)
+
+@app.route("/product", methods=["GET"])
+def get_all():
+    all_products = Product.query.all()
+    return jsonify(products_schema.dump(all_products).data)
+
+@app.route("/product/<id>", methods=["GET"])
+def get_single(id):
+    product = Product.query.get(id)
+    return product_schema.jsonify(product)
+
+
+@app.route("/product/<id>", methods=['PUT'])
+def update_product(id):
+    product = Product.query.get(id)
+
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    qty = request.json['qty']
+
+    product.name = name
+    product.description = description
+    product.price = price
+    product.qty = qty
+
+    db.session.commit()
+    return product_schema.jsonify(product)
+
+@app.route("/product/<id>", methods=["DELETE"])
+def del_single(id):
+    product = Product.query.get(id)
+    db.session.delete(product)
+    db.session.commit()
+    
+    return product_schema.jsonify(product)
 
 # run server
 if __name__ == "__main__":
